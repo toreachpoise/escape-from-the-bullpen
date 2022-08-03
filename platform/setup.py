561b06452708b6e8d15ca0f7953da9a4ab4015e3
toreachpoise <96789614+toreachpoise@users.noreply.github.com>
2022-08-03 12:53:59 -0700

    + packaged demo version, level 2, just some tidy up

---
+++ platform/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Escape the Bullpen",
+        version="0.1",
+        description="a queer indie platformer",
+        author="Aaron El Sabrout",
+        author_email="elsabrout32@gmail.com",
+        url = "https://toreachpoise.itch/io/escape-the-bullpen",
+        packages=['platform'])
